f04760ab55a803012ebc6fef80b27efb1611db53
Thibault Clérice <thibault.clerice@inria.fr>
2025-10-30 14:02:50 +0100

    Clean up

--- files.sh
+++
@@ -1,7 +0,0 @@
-awk 'BEGIN{jpg=0; xml=0} 
-     /\.jpg$/ {jpg++} 
-     /\.xml$/ {xml++} 
-     END{print "JPG:", jpg, "XML:", xml, "DIFF:", jpg - xml}' < <(
-  find . -mindepth 2 -maxdepth 2 \( -iname '*.jpg' -o -iname '*.xml' \)
-)
-
